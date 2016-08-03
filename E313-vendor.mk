a74b0973f171971b4df95728e7fa6bf5a09f2e78
tandob-mtk <meetsoumen.das1@gmail.com>
2016-08-03 21:22:41 +0530

    added files of stock kk for E313

---
+++ E313-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/E313/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/E313/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/E313/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/E313/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/E313/proprietary/vendor,system/vendor)
